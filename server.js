3ba0a5184a794c4b2db393fbd30dbc85fdd4d5e0
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-05-13 11:32:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000; // Port aplikasi kamu
 
-// Mengatur folder publik untuk file statis (HTML, CSS, JS)
+// Middleware untuk membaca file statis seperti CSS/Gambar di folder root
 app.use(express.static(path.join(__dirname, '/')));
 
+// Menampilkan index.html saat domain diakses
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Website berjalan di http://localhost:${port}`);
+  console.log(`Website xyzid.store jalan di port ${port}`);
 });
